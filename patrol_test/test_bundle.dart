5c9f805b9b7e94b68c3fa7c02c91ecb77a44b9da
BBrunoF <brunofreitas0465@gmail.com>
2026-05-11 14:58:40 +0100

    Reorder test imports and groups for better organization

--- patrol_test/test_bundle.dart
+++ patrol_test/test_bundle.dart
@@ -9,13 +9,14 @@ import 'package:patrol/src/platform/contracts/contracts.dart';
 import 'package:test_api/src/backend/invoker.dart';
 
 // START: GENERATED TEST IMPORTS
-import 'login_test.dart' as login_test;
-import 'job_history_test.dart' as job_history_test;
 import 'job_detail_test.dart' as job_detail_test;
-import 'request_test.dart' as request_test;
-import 'pro_profile_test.dart' as pro_profile_test;
+import 'job_history_test.dart' as job_history_test;
+import 'login_test.dart' as login_test;
 import 'pro_jobs_test.dart' as pro_jobs_test;
+import 'pro_profile_test.dart' as pro_profile_test;
 import 'profile_test.dart' as profile_test;
+import 'request_test.dart' as request_test;
+import 'smoke_test.dart' as smoke_test;
 // END: GENERATED TEST IMPORTS
 
 Future<void> main() async {
@@ -78,13 +79,14 @@ Future<void> main() async {
   });
 
 // START: GENERATED TEST GROUPS
-  group('login_test', login_test.main);
-  group('job_history_test', job_history_test.main);
   group('job_detail_test', job_detail_test.main);
-  group('request_test', request_test.main);
-  group('pro_profile_test', pro_profile_test.main);
+  group('job_history_test', job_history_test.main);
+  group('login_test', login_test.main);
   group('pro_jobs_test', pro_jobs_test.main);
+  group('pro_profile_test', pro_profile_test.main);
   group('profile_test', profile_test.main);
+  group('request_test', request_test.main);
+  group('smoke_test', smoke_test.main);
 // END: GENERATED TEST GROUPS
 
   final dartTestGroup = await testExplorationCompleter.future;
